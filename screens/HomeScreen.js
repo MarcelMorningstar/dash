4dd5f1a40741a73de6eddb0b82443969dcdda445
Roland <90729098+MarcelMorningstar@users.noreply.github.com>
2023-01-28 15:05:30 +0200

    React Native Maps

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,36 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import { useSelector } from 'react-redux'
+import { selectOrigin } from '../slices/mainSlice'
+import MapView from 'react-native-maps'
 
 export default function HomeScreen() {
+  const origin = useSelector(selectOrigin)
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <MapView
+        initialRegion={{
+          latitude: origin.location.lat,
+          longitude: origin.location.lng,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+        mapType='mutedStandard'
+        style={styles.map}
+      >
+
+      </MapView>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  map: {
+    width: '100%',
+    height: '100%'
+  }
+})
